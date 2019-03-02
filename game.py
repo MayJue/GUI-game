918fd669df78a1152aba772485b23b3f542176a9
mayjue <juem@berea.edu>
2019-03-02 15:22:51 -0500

    added started code

--- game.py
+++ game.py
@@ -2,8 +2,10 @@
 from tkinter import *
 
 screen = Tk()
+screen.minsize(width=250, height=100)
+screen.maxsize(width=250, height=100)
 
-say = Label(screen, text="")
+say = Label(screen, text="Hi, we are starting a game")
 say.pack()
 
 screen.mainloop()

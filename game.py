0699c1646f389b07977d26e051da8267337a3959
mayjue <juem@berea.edu>
2019-03-02 21:46:29 -0500

    functions

--- game.py
+++ game.py
@@ -8,8 +8,8 @@ class Game:
         self.frame.grid()
 
         # print the question
-        self.message = Label(self.frame, text="What is your name?")
-        self.message.grid(row=0)
+        self.message = NONE
+
         # Box for input
         self.answer = Entry(self.frame)
         self.answer.grid(row=0, column=1)
@@ -17,20 +17,34 @@ class Game:
         self.printingmessage = StringVar()
         self.label = NONE
 
-        # submit button                                    # call function
-        self.showbutton = Button(self.frame, text="submit", command=self.AskAnswer)
-        self.showbutton.grid(row=1)
+        # submit button
+        self.showbutton = NONE
+
         #
         # self.quitbutton = Button(frame, text="quit", command=frame.quit)
         # self.quitbutton.grid(row=2)
+
+    def Create_message(self, print=" "):
+        """
+        Print out whatever massage
+        """
+        self.message = Label(self.frame, text=print)
+        self.message.grid(row=0)
+
+    def button1(self):
+        """
+        Create the submit button on screen
+        """                                                 # call function
+        self.showbutton = Button(self.frame, text="submit", command=self.AskAnswer)
+        self.showbutton.grid(row=0, column=2)
 
     def AskAnswer(self):
         """
         When button is pressed, get the item from the textbox
         """
-        txt = self.answer.get()     # get item from user input box
-        message = "Hi, {0}!".format(txt)    # create a massage with input
-        self.printingmessage.set(message)      # printingmessage set to message
+        txt = self.answer.get()    # get item from user input box
+        M = "Hi, {0}! Welcome to the game of nim!".format(txt)    # create a massage with input
+        self.printingmessage.set(M)      # printingmessage set to message
 
     def PrintAnswer(self, response=""):
         """
@@ -46,6 +60,8 @@ screen.minsize(width=550, height=100)
 screen.maxsize(width=550, height=100)
 
 G = Game(screen)    # call class
+G.Create_message("what is your name?")
+G.button1()
 G.PrintAnswer()     # call function to print
 
 # say = Label(screen, text="What's your name?")
